b50096f50e809457d6effe21abf2efb7ba73cf45
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-09-06 14:09:44 -0500

    Create get-result.php

---
+++ get-result.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle= "Get Result";
+Include "view-header.php";
+?>
+    <h1>Get Result</h1>
+<?php
+if (isset($_GET['my-name'])) {
+?>
+  <p>The value sent is:</p>
+<?php
+  echo $_GET['my-name];
+} else {
+?>
+  <p>Nothin sent to the page</p>
+<?php
+}
+
+Include "view-footer.php";
+?>
