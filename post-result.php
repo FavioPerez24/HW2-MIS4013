b856bb025c9cf08bd8e9c99509aff4c1ca50fdf3
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-09-11 14:07:01 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -9,7 +9,7 @@ Include "view-footer.php";
 
 function getDisplay(){
     if (isset($_POST['my-name'])) {
-  return "<p>The value sent is:</p>" . $_POST['my-name];
+  return "<p>The value sent is:</p>" . $_POST['my-name'];
 } else {
   return "<p>Nothing posted to the page.</p>";
 }
